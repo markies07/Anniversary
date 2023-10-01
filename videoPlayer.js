01351e0cb7eefc42abc105102efda87814cc6e99
Markies <markchristiannaval07@gmail.com>
2023-10-01 23:47:45 +0800

    youtube embeded

--- videoPlayer.js
+++ videoPlayer.js
@@ -1,12 +1,7 @@
 let videoPlayer = document.getElementById("videoPlayer");
 let myVideo = document.getElementById("myVideo");
 
-function stopVideo() {
-  videoPlayer.style.display = "none";
-  myVideo.pause();
-}
 
-function playVideo(file) {
-  myVideo.src = file;
+function playVideo() {
   videoPlayer.style.display = "block";
 }

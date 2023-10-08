39572b5fe85f6f5b3295e560fe2c63eb0527b00c
Markies <markchristiannaval07@gmail.com>
2023-10-08 19:22:51 +0800

    add

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ document.getElementById("button").addEventListener("click", function(event){
     var date = document.getElementById("date").value.toLowerCase();
     
     if (date == "") {
-        document.getElementById("msg").innerHTML = "Text field is blank";
+        document.getElementById("msg").innerHTML = "Please enter a value!";
         setTimeout(function() {
         document.getElementById("msg").innerHTML = "";
         }, 2000);
     } else if(date == "october 13, 2022") {
-        document.getElementById("msg").innerHTML = "Success!";
+        document.getElementById("msg").style = "color: green; font-weight: bold;";
+        document.getElementById("msg").innerHTML = "Correct!";
         setTimeout(function() {
             document.getElementById("msg").innerHTML = "";
             window.location.assign("main.html");
         }, 1500);
     } else {
-        document.getElementById("msg").innerHTML = "Incorrect!";
+        document.getElementById("msg").innerHTML = "Wrong Input!";
         setTimeout(function() {
             document.getElementById("msg").innerHTML = "";
         }, 2000); 
